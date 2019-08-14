c74832bc738bbfefb30f4510e733b139196670af
gouzicai <5115549+caojihua@user.noreply.gitee.com>
2019-08-14 16:12:29 +0800

    lazylod

---
+++ index.js
@@ -0,0 +1,26 @@
+
+//获取图片地址img[i].dataset.src
+// window.getBoundingClientRect()可以获得元素的宽高和离html顶部，底部，左边框，有边框的距离
+function imgonload(){
+    let img = document.querySelectorAll('img');
+    for(let i = 0; i < img.length;i++){
+        if(img[i].getBoundingClientRect().top < window.innerHeight){
+            // console.log(img[i].getBoundingClientRect().top)
+            // console.log(img[i].dataset.src);
+            img[i].src=img[i].dataset.src;
+        }
+        
+    }
+    // console.log(img);
+}
+
+
+function scollImg(){
+    let _this = this;
+    return function(){
+        // fn.apply(_this);
+        setTimeout(()=>{
+            this.imgonload.apply(_this);
+        },500)
+    }
+}
